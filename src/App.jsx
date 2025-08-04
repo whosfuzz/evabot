097c2586e5244efd3ccd12023ca9f1e39bb838c7
whosfuzz <whosfuzz29@gmail.com>
2025-08-04 10:25:46 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useUser } from '../lib/context/user';
-import { useLocation } from 'react-router-dom';
 import { FaTimes, FaFilter, FaEdit, FaTrash, FaUser, FaMoon, FaSun } from 'react-icons/fa';
 
 function App() {
   const { user, error, setError, documents, total, createDocument, updateDocument, deleteDocument, login, logout, setIsDarkMode, isDarkMode, goHome } = useUser();
-  const location = useLocation();
   
   const [filters, setFilters] = useState({
     folder: '',
@@ -27,7 +25,7 @@ function App() {
   };
 
   useEffect(() => {
-    const params = new URLSearchParams(location.search);
+    const params = new URLSearchParams(window.location.search);
     setFilters({
       folder: params.get('folder') || '',
       message: params.get('message') || '',
@@ -36,7 +34,7 @@ function App() {
       page: Number(params.get('page')) || 1,
       limit: Number(params.get('limit')) || 10,
     });
-  }, [location.search]);
+  }, []);
 
   useEffect(() => {
     const handleKeyDown = (e) => e.key === 'Escape' && closeModal();
@@ -79,7 +77,8 @@ function App() {
       : window.location.pathname;
     
     window.history.pushState({}, '', newUrl);
-    setFilters(newFilters);
+    window.location.reload();
+    //setFilters(newFilters);
   };
 
   const applyFilters = () => {
